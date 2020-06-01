c435013213e881f196a7664b6b36104f602bfe82
jujiankang <jiank6661@163.com>
2020-06-01 09:01:48 +0800

    merge

--- src/conf/index.js
+++ src/conf/index.js
@@ -1,9 +1,9 @@
 const dev = {
-	// 开发
-	// 接口访问地址
-	baseUrl: 'http://localhost:8088'
+    // 开发
+    // 接口访问地址
+    baseUrl: 'http://localhost:8088'
 }
 
 export function def() {
-	return dev
+    return dev
 }
