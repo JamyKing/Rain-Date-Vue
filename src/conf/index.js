2392981e6a39808d9a6fe7d8bc300f6285d0ed2f
jujiankang <jiank6661@163.com>
2020-06-02 17:55:16 +0800

    线上访问地址

--- src/conf/index.js
+++ src/conf/index.js
@@ -1,8 +1,8 @@
 const dev = {
 	// 开发
-	baseUrl: 'http://localhost:8080'
+	// baseUrl: 'http://localhost:8080'
     // 线上
-	// baseUrl: 'http://jianking.vip:8080'
+	baseUrl: 'http://www.jianking.vip'
 }
 
 export function def() {
