93f0a2e4aff0d5fea940e79e2f64ee5e5f640cf2
iRainy6661 <JianK6661@163.com>
2020-07-08 20:10:39 +0800

    update

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -27,12 +27,12 @@ export function timeFilter (time = +new Date()) {
 }
 
 export function randomNum (minNum, maxNum) {
-    switch(arguments.length){
+    switch (arguments.length) {
         case 1:
-            return parseInt(Math.random()*minNum+1,10)
+            return parseInt(Math.random() * minNum + 1,10)
             break
         case 2:
-            return parseInt(Math.random()*(maxNum-minNum+1)+minNum,10)
+            return parseInt(Math.random() * (maxNum - minNum + 1) + minNum,10)
             break
         default:
             return 0
